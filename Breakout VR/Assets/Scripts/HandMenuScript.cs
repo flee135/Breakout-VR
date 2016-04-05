b6033c5e210429b9b1e3b1c35df5eb745195e256
Felix Lee <felix.lee135@gmail.com>
2016-04-05 19:03:14 -0400

    Added menu

---
+++ Breakout VR/Assets/Scripts/HandMenuScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Leap;
+
+public class HandMenuScript : MonoBehaviour {
+
+    public GameObject menu;
+    public Text breakoutText;
+    public Text exitText;
+
+    Controller controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = new Controller();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Frame frame = controller.Frame();
+        List<Hand> hands = frame.Hands;
+        Hand leftHand = null;
+        Hand rightHand = null;
+        foreach (Hand hand in hands)
+        {
+            if (hand.IsLeft) leftHand = hand;
+            else if (hand.IsRight) rightHand = hand;
+        }
+
+        if (menu.activeSelf)
+        {
+            handleMenu(rightHand);
+        }
+	}
+
+    private void handleMenu(Hand rightHand)
+    {
+        if (rightHand == null)
+        {
+            breakoutText.color = Color.black;
+            exitText.color = Color.black;
+            return;
+        }
+
+        Vector position = rightHand.PalmPosition;
+        if (position.z < 0) // Top
+        {
+            breakoutText.color = Color.red;
+            exitText.color = Color.black;
+
+            if (rightHand.PalmPosition.y > 300)
+            {
+                menu.SetActive(false);
+            }
+        }
+        else
+        {
+            breakoutText.color = Color.black;
+            exitText.color = Color.red;
+
+            if (rightHand.PalmPosition.y > 300)
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
