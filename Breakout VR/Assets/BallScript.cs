0b257231681ee528e9038ffc1f8b82f5bd5b342f
lukazhupa <luka.zhupa@gmail.coml>
2016-04-12 20:19:44 -0400

    Prefab folder added

--- Breakout VR/Assets/BallScript.cs
+++ Breakout VR/Assets/BallScript.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class BallScript : MonoBehaviour {
 
+	public float thrust;
+	public Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(Vector3.up * thrust );
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
