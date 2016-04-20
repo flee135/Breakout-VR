ac5e8943ea153f5bbce8eee6234b926d1b00be35
rstudio <rstudio@example.com>
2016-04-20 13:36:30 -0400

    Basic Functionality

---
+++ Breakout VR/Assets/Scripts/FieldScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldScript : MonoBehaviour {
+
+	private Material origMat;
+	private bool isFading = false;
+	private float fadeTime = 0.075f;
+
+	void Start() {
+
+		origMat = gameObject.GetComponent<Renderer> ().material;
+
+	}
+
+	void OnCollisionEnter(Collision other) {
+
+		if (other.gameObject.CompareTag ("Ball")) {
+
+			Material origMat = gameObject.GetComponent<Renderer> ().material;
+			Color finalColor = origMat.color, startColor = finalColor;
+
+			if (isFading == false) {
+
+				isFading = true;
+				StartCoroutine (fade ());
+
+			} else {
+				
+				StopAllCoroutines();
+				StartCoroutine (fade ());
+
+			}
+
+		}
+
+	}
+
+	Color setAlpha (float alphaLevel) {
+
+		return new Color (origMat.color.r, origMat.color.g, origMat.color.b, alphaLevel);
+
+	}
+
+	IEnumerator fade () {
+		
+		Color finalColor = origMat.color;
+		float alphaLevel = 1.0f;
+
+		while (alphaLevel > 0.0f) {
+
+			origMat.color = setAlpha(alphaLevel -= 0.05f);
+
+			if (alphaLevel < 0.05f) {
+
+				isFading = false;
+				origMat.color = setAlpha(0.0f);
+				StopCoroutine (fade());
+				break;
+
+			}
+
+			yield return new WaitForSeconds (fadeTime);
+
+		}
+
+	}
+
+}
---
+++ Breakout VR/Assets/Scripts/SceneController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+	public GameObject bluePrefab, greenPrefab, yellowPrefab, orangePrefab, redPrefab;
+
+	void Start () {
+
+		for (int i = -4; i <= 4; i++) {
+
+			for (int j = -1; j <= 2; j++) {
+
+				Instantiate (bluePrefab, new Vector3 (2.0f * i, j * 2, -6.0f), Quaternion.identity);
+				Instantiate (greenPrefab, new Vector3 (2.0f * i, j * 2, -4.0f), Quaternion.identity);
+				Instantiate (yellowPrefab, new Vector3 (2.0f * i, j * 2, -2.0f), Quaternion.identity);
+				Instantiate (orangePrefab, new Vector3 (2.0f * i, j * 2, 0.0f), Quaternion.identity);
+				Instantiate (redPrefab, new Vector3 (2.0f * i, j * 2, 2.0f), Quaternion.identity);
+
+			}
+
+		}
+
+	}
+
+}
