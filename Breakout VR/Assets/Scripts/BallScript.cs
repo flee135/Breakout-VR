7698f654a7a2638fbf3222655239f51ed7eb6a1d
Felix Lee <felix.lee135@gmail.com>
2016-05-02 20:24:31 -0400

    Adding audio when hitting block

--- Breakout VR/Assets/Scripts/BallScript.cs
+++ Breakout VR/Assets/Scripts/BallScript.cs
@@ -5,19 +5,18 @@ public class BallScript : MonoBehaviour {
 
 	private Rigidbody rb;
     public float thrust;
+    public AudioClip boxAudio;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
-		//rb.AddForce(new Vector3(1.0f, 1.0f, 1.0f) * thrust);
 	}
 
 	void OnCollisionEnter(Collision other) {
 
 		if (other.gameObject.CompareTag ("Block")) {
-
 			Destroy (other.gameObject);
-
+            AudioSource.PlayClipAtPoint(boxAudio, gameObject.transform.position);
 		}
 
 	}
